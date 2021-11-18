37b40c211b87f832c060b1b6ed250b166e74935e
Abena <72131115+BlackPhinix@users.noreply.github.com>
2021-11-18 18:57:16 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,4 @@
+private void selectNumber(int x, int y)   //called method on mouse click
+public Dimension getPreferredSize()  //set the preferred size of display panel
+protected void paintComponent(Graphics g)  //called whenever the display panel needs painting
+public void actionPerformed(ActionEvent e)  //call method for push button selected
